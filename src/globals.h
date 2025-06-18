520e2896bcf1a5b99438a96058023795d136fbe5
Moriaan Michiels <moriaan.michiels@gmail.com>
2025-06-18 21:45:02 +0200

    Initial commit

---
+++ src/globals.h
@@ -0,0 +1,23 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define HACKCLUB_URL "https://ai.hackclub.com/chat/completions"
+
+
+#endif // GLOBALS_H
+
+
+
+/*
+ * Example request (JSON):
+ *
+ * {
+ *   "messages": [
+ *     {
+ *       "role": "user",
+ *       "content": "test"
+ *     }
+ *   ]
+ * }
+ *
+ */
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cjson/cJSON.h>
+#include <curl/curl.h>
+
+#include "globals.h"
+
+int main() {
+	printf("IDEAGENERATOR START\n");
+
+	printf("Hackclub url: %s\n", HACKCLUB_URL);
+}
